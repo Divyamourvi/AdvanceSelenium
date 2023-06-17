e009f8dc464bf68ee311980e0018f8165b2767aa
Vinay G <Vinay G@Sandeepa-arecl>
2023-06-17 18:04:10 +0530

    TC_001 branch committed

--- src/test/java/practice/Demo2.java
+++ src/test/java/practice/Demo2.java
@@ -5,11 +5,9 @@ public class Demo2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("byeeee");
-<<<<<<< HEAD
-System.out.println("Auto engi");
-=======
+
 System.out.println("Welcome");
->>>>>>> branch 'master' of https://github.com/Divyamourvi/AdvanceSelenium.git
+System.out.println("branching");
 	}
 
 }
